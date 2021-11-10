44a8bc4c35c52ba3bdc7ff33b681253b590300ff
inayamatatsuya <f2196012@nishogakusha-u.ac.jp>
2021-11-10 18:08:27 +0900

    first commit

---
+++ Desktop/python/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import time
+st.title("streamlit 超入門")
+
+
+st.write("プログレスバーの表示")
+"start"
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iterarion{i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+"DONE!"
